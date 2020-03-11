3b97838bbace108eac5b9e58c4a98743b2f52bb7
rayane tahiri <rayanetahiri@MacBook-Pro-de-rayane.local>
2020-03-11 17:24:26 +0100

    ajout du controllerCategorie et route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,42 @@
 <?php
 
+  
+
 use Illuminate\Support\Facades\Route;
 
+  
+
 /*
+
 |--------------------------------------------------------------------------
+
 | Web Routes
+
 |--------------------------------------------------------------------------
+
 |
+
 | Here is where you can register web routes for your application. These
+
 | routes are loaded by the RouteServiceProvider within a group which
+
 | contains the "web" middleware group. Now create something great!
+
 |
+
 */
 
+  
+
 Route::get('/', function () {
-    return view('welcome');
+
+return  view('welcome');
+
 });
+
+Route::get("","CategorieController@index")->name("categorie");
+Route::get("", "CategorieController@create")->name("addCategorie");
+Route::post("", "CategorieController@store")->name("saveCategorie");
+Route::get("/{id}", "CategorieController@edit")->name("editCategorie");
+Route::post("/{id}", "CategorieController@update")->name("updateCategorie");
+Route::get("/{id}", "CategorieController@destroy")->name("deleteCategorie");
