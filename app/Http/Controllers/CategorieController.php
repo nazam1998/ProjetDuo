cf2c237cf6ab7e48060d08d05f790bd276ef0dec
rayane tahiri <rayanetahiri@MacBook-Pro-de-rayane.local>
2020-03-11 16:42:06 +0100

    ajout du controllerCategorie a terminer

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Categorie;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $categorie = Categorie::all();
+        return view ("", compact("categorie"));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("");
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $categorie = new User();
+        $categorie->=$request->input("");
+        $categorie->=$request->input("");
+        $categorie->=bcrypt($request->input(""));
+ 
+        $categorie->save();
+        return redirect()->route("");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $categorie=Categorie::find($id);
+        return view('', compact('categorie'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $categorie = Photo::find($id);
+        $categorie->=$request->input("");
+        $categorie->=$request->input("");
+        $categorie->=$request->input("");
+        $categorie->=$request->input("");
+        $categorie->save();
+        return redirect()->route("");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+        $categorie=Photo::find($id);
+        $categorie->delete();
+        return redirect()-> route("");
+        //
+    }
+}
