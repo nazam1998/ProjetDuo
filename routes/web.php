60e28d5d05df1ec7c97cda28ae919f8c344b5e12
Nazam <nazam98-be@email.com>
2020-03-11 17:13:53 +0100

    added avatarcontroller

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/admin/avatar','AvatarController@index')->name('avatar');
+Route::get('/admin/addAvatar','AvatarController@create')->name('addAvatar');
+Route::post('/admin/saveAvatar','AvatarController@store')->name('saveAvatar');
+Route::get('/admin/editAvatar/{id}','AvatarController@edit')->name('editAvatar');
+Route::post('/admin/updateAvatar/{id}','AvatarController@update')->name('updateAvatar');
+Route::get('/admin/deleteAvatar/{id}','AvatarController@destroy')->name('deleteAvatar');
