4640df4376d1d6b8cd5a719d7a0166b31f4ab55f
rayane tahiri <rayanetahiri@MacBook-Pro-de-rayane.local>
2020-03-12 09:24:24 +0100

    ajout controller categorie

---
+++ resources/views/admin/categorie/add.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+
+@section('title','Avatar')
+
+@section('content')
+    
+
+
+
+
+@endsection
--- resources/views/admin/categorie/index.blade.php
+++ resources/views/admin/categorie/index.blade.php
@@ -1,11 +1,31 @@
 @extends('layout.main')
 
-@section('title','Avatar')
+@section('title','Categorie')
 
 @section('content')
+    <table>
+        <thead>
+            <tr>
+                <th colspan="4">Table Avatar</th>
+            </tr>
+            <tr>
+                <th>ID</th>
+                <th>Categorie</th>
     
-
-
-
-
+                <th colspan="2">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($categories as $categorie)
+                
+            <tr>
+            <td>{{$item->id}}</td>
+            <td>{{$item->nom}}</td>
+            <td>{{$item->image}}</td>
+            <td><a href="{{route('editCategorie',$item->id)}}"><button class="btn btn-warning">Edit</button></a></td>
+            <td><a href="{{route('deleteCategorie',$item->id)}}"><button class="btn btn-danger">Delete</button></a></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
