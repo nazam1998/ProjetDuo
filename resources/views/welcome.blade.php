f636adb3ca33bae18a4497e15bae5037863e4cb3
rayane tahiri <rayanetahiri@MacBook-Pro-de-rayane.local>
2020-03-13 09:46:39 +0100

    debut du design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,12 @@
 @section('title','Accueil')
 @section('content')
 
-<div class="container text-center">
-    <h1>Welcome</h1>
+<div class="header text-center">
+    <h1 class="text-center">Projet Nazam et Rayane</h1>
+
+
+
+
 </div>
+
 @endsection
